0f4c3d1d1feed51e0187b93d10f5fd24d21603c1
Edtiger1905 <verytiger1905@gmail.com>
2025-07-10 14:27:10 +0200

    Update printer.c

--- printer.c
+++ printer.c
@@ -1,106 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printer.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epandele <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/09 10:37:38 by epandele          #+#    #+#             */
+/*   Updated: 2025/07/10 07:19:19 by epandele         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-// Funzione per stampare un intero
-int print_int(int i)
+int     print_int(int i)
 {
-    int char_count = 0;
+        int     char_count;
 
-    if (i < 0) {
-        ft_putchar('-');
-        char_count++;
-        char_count += print_unsigned((unsigned int)(-i));
-    } else {
-        char_count += print_unsigned((unsigned int)i);
-    }
-    return char_count;
+        char_count = 0;
+        if (i < 0)
+        {
+                ft_putchar('-');
+                char_count++;
+                char_count += print_unsigned((unsigned int)(-i));
+        }
+        else
+        {
+                char_count += print_unsigned((unsigned int)i);
+        }
+        return (char_count);
 }
 
-// Funzione per stampare una stringa
-int print_string(const char *str)
+int     print_string(const char *str)
 {
-    int char_count = 0;
+        int     char_count;
 
-    if (str == NULL) {
-        str = "(null)";
-    }
-    while (*str) {
-        ft_putchar(*str);
-        char_count++;
-        str++;
-    }
-    return char_count;
+        char_count = 0;
+        if (str == NULL)
+        {
+                str = "(null)";
+        }
+        while (*str)
+        {
+                char_count += ft_putchar(*str);
+                str++;
+        }
+        return (char_count);
 }
 
-/* Funzione per stampare un puntatore
-int print_pointer(void *ptr)
+int     print_pointer(void *ptr)
 {
-    unsigned long address = (unsigned long)ptr;
-    int char_count = 0;
+        unsigned long   address;
+        int                             char_count;
 
-    if(ptr != NULL)
-    {
+        address = (unsigned long)ptr;
+        char_count = 0;
+        if (ptr == NULL)
+        {
+                write(1, "(nil)", 5);
+                return (5);
+        }
         ft_putchar('0');
         ft_putchar('x');
         char_count += 2;
-    }
-    char_count += print_hex(address, 0);
-    return char_count;
-}*/
-int print_pointer(void *ptr)
-{
-    unsigned long address = (unsigned long)ptr;
-    int char_count = 0;
-
-
-    if (ptr == NULL)
-    {
-        write(1,'(nil)',5);
-        char_count += 1;
-    }
-
-    ft_putchar('0');
-    ft_putchar('x');
-    char_count += 2;
-    char_count += print_hex(address, 0);
-
-    return char_count;  
+        char_count += print_hex(address, 0);
+        return (char_count);
 }
 
-
-// Funzione per stampare un numero senza segno
-int print_unsigned(unsigned int val)
+int     print_unsigned(unsigned int val)
 {
-    int char_count = 0;
+        int     char_count;
 
-    if (val >= 10) {
-        char_count += print_unsigned(val / 10);
-    }
-    ft_putchar('0' + (val % 10));
-    char_count++;
-    return char_count;
+        char_count = 0;
+        if (val >= 10)
+        {
+                char_count += print_unsigned(val / 10);
+        }
+        char_count += ft_putchar('0' + (val % 10));
+        return (char_count);
 }
 
-// Funzione per stampare un numero esadecimale
-int print_hex(unsigned long value, int uppercase)
+int     print_hex(unsigned long value, int uppercase)
 {
-    int char_count = 0;
-    char digit;
+        int             char_count;
+        char    digit;
 
-    if (value >= 16) {
-        char_count += print_hex(value / 16, uppercase);
-    }
-
-    // Ottieni il digit corrente
-    if ((value % 16) < 10) {
-        digit = '0' + (value % 16);
-    } else {
-        if (uppercase)
-            digit = 'A' + ((value % 16) - 10);
+        char_count = 0;
+        if (value >= 16)
+        {
+                char_count += print_hex(value / 16, uppercase);
+        }
+        if ((value % 16) < 10)
+        {
+                digit = '0' + (value % 16);
+        }
         else
-            digit = 'a' + ((value % 16) - 10);
-    }
-
-    ft_putchar(digit);
-    char_count++;
-    return char_count;
+        {
+                if (uppercase)
+                        digit = 'A' + ((value % 16) - 10);
+                else
+                        digit = 'a' + ((value % 16) - 10);
+        }
+        char_count += ft_putchar(digit);
+        return (char_count);
 }
